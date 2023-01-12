ee7020d4b5cfc0234a6db79cf7c73436751d98da
Gustavo Figueiredo <gustavopfp.trab@gmail.com>
2023-01-12 10:45:08 -0300

    login flow and users impleted

--- stocks.py
+++ stocks.py
@@ -10,6 +10,12 @@ class Stock:
         self.price = price
         self.market_cap = market_cap
 
+class User:
+    def __init__(self, name, nickname, password):
+        self.name = name
+        self.nickname = nickname
+        self.password = password
+
 
 
 
@@ -17,9 +23,19 @@ itub4 = Stock('Itaú Unibanco', 'ITUB4', 25.87, 238.03)
 bbdc4 =  Stock('Bradesco', 'BBDC4', 15.19, 152.37 )
 nubr33 = Stock('Nu Holdings Ltd', 'NUBR33', 3.10, 86.73)
 stock_list = [itub4, bbdc4, nubr33]
+
+user1 = User('Gustavo', 'gustapfp', '1234')
+user2 = User('Maria','mary', '1234')
+
+users_list = {
+    user1.nickname:user1,
+    user2.nickname:user2
+}
         
 app = Flask(__name__) 
 app.secret_key = 'teste'
+
+
 
 @app.route('/')
 def home():
@@ -27,8 +43,9 @@ def home():
 
 @app.route('/new_stock')
 def new_stock():
-    if 'user_log' not in session or session['user_log'] == None:
-        return redirect(('login'))
+    if session['user_log'] not in session or session['user_log'] == None:
+        print("oi")
+        return redirect(url_for('login', proxima=url_for('new_stock')))
     return render_template("new_stock.html")
 
 @app.route('/create_stock', methods=['POST', ])
@@ -45,21 +62,30 @@ def create_stock():
 
 @app.route('/login')
 def login():
-    return render_template("login.html")
+    next_page = request.args.get('next')
+    return render_template("login.html", next=next_page)
+
 
 @app.route('/autentification', methods=['POST', ])
 def autentification():
     username = request.form['username']
     password = request.form['password']
 
-    if username == 'guga' and password == 'teste':
-        session['user_log'] = request.form['username']
-        flash(session['user_log'] + ' login succeed.')
-        return redirect(url_for('home'))
+    if username in users_list:
+        user = users_list[username]
+        if password == users_list[username].password:
+            session['user_log'] = user.nickname
+            flash(user.nickname + ' login succeed.')
+            next_page = request.form['next']
+            return redirect(next_page)
+    else:
+        flash('Login invalid!')
+        return redirect(url_for('login'))
 
-    flash(session['user_log'] + ' login invalid!')
-    return redirect('/login')
-
-
+@app.route('/logout')
+def logout():
+    session['user_log'] == None
+    flash('Logout succeed!')
+    return redirect(url_for('home'))
 
 app.run(debug=True)
